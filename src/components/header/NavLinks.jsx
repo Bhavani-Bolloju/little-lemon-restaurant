d16043e15aaa587b40fec1e0ca858aa28ae8fe41
Bhavani Bolloju <bhavani.bolloju@gmail.com>
2023-10-25 15:12:24 +0530

    add calltoaction styling

--- src/components/header/NavLinks.jsx
+++ src/components/header/NavLinks.jsx
@@ -1,15 +1,26 @@
 import React from "react";
+import classes from "./NavLinks.module.scss";
 
 function NavLinks() {
   return (
-    <nav>
+    <nav className={classes.nav}>
       <ul>
         <li>
           <a href="#home">home</a>
+        </li>
+        <li>
           <a href="#about">about</a>
+        </li>
+        <li>
           <a href="#menu">menu</a>
+        </li>
+        <li>
           <a href="#reservation">reservation</a>
+        </li>
+        <li>
           <a href="#order">order online</a>
+        </li>
+        <li>
           <a href="#login">login</a>
         </li>
       </ul>
--- src/components/main/CallToAction.jsx
+++ src/components/main/CallToAction.jsx
@@ -1,16 +1,27 @@
 import React from "react";
 import classes from "./CallToAction.module.scss";
+import heroImage from "../../assets/little-lemon-assets/restauranfood.jpg";
 
 function CallToAction() {
   return (
     <section className={classes.callToAction}>
-      <h1 className={classes["callToAction__title"]}>little lemon</h1>
-      <p className={classes["callToAction__subTitle"]}>chicago</p>
-      <p className={classes["callToAction__text"]}>
-        We are a family owned Mediterranean restaurant, focused on traditional
-        recipes served with a modern twist
-      </p>
-      <button className={classes["callToAction__btn"]}>reserve a table</button>
+      <div className={classes["callToAction__content"]}>
+        <h1 className={classes["callToAction__title"]}>little lemon</h1>
+        <p className={classes["callToAction__subTitle"]}>chicago</p>
+        <p className={classes["callToAction__text"]}>
+          We are a family owned Mediterranean restaurant, focused on traditional
+          recipes served with a modern twist. At Little Lemon, tradition and
+          innovation come together to offer a dining experience that's both
+          familiar and exciting.
+        </p>
+
+        <button className={classes["callToAction__btn"]}>
+          reserve a table
+        </button>
+      </div>
+      <div className={classes["callToAction__image"]}>
+        <img src={heroImage} alt="" />
+      </div>
     </section>
   );
 }
