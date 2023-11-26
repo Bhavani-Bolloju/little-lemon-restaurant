409272beb3939b22481dc683d5241eb06661dffd
Bhavani Bolloju <bhavani.bolloju@gmail.com>
2023-11-26 20:23:28 +0530

    optimize pages by adding lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
 import { Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
+// import HomePage from "./pages/HomePage";
 import BookingPage from "./pages/BookingPage";
 import BookingConfirmationPage from "./pages/BookingConfirmationPage";
+import { lazy, Suspense } from "react";
+
+const Home = lazy(() => import("./pages/HomePage"));
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/booking" element={<BookingPage />} />
-      <Route path="/bookingConfirm/:id" element={<BookingConfirmationPage />} />
+      <Route
+        path="/"
+        element={
+          <Suspense fallback={<div>Loading...</div>}>
+            <Home />
+          </Suspense>
+        }
+      />
+      <Route
+        path="/booking"
+        element={
+          <Suspense fallback={<div>Loading...</div>}>
+            <BookingPage />
+          </Suspense>
+        }
+      />
+      <Route
+        path="/bookingConfirm/:id"
+        element={
+          <Suspense fallback={<div>Loading...</div>}>
+            <BookingConfirmationPage />
+          </Suspense>
+        }
+      />
     </Routes>
   );
 }
